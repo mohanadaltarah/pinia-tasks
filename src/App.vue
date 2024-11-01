a1d86aeb1690f09b52e5a4ab4bc1622bf226c05c
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-11-01 16:44:50 +0300

    Access task store and show the tasks

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
     <!--  task list  -->
     <div class="task-list">
       <div v-for="task in taskStore.tasks" :key="task.id">
-        <p>{{ task.title }}</p>
+        <TaskDetails :task="task"/>
       </div>
     </div>
   </main>
@@ -19,6 +19,7 @@
 
 <script setup>
 import {useTaskStore} from "@/stores/TaskStore.js";
+import TaskDetails from "@/components/TaskDetails.vue";
 
 const taskStore = useTaskStore();
 
