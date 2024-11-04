df00ffe1bb789c900176ed6c5765ebbeabc266c7
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-11-04 17:44:51 +0300

    Create task deletion and like toggle

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -2,13 +2,15 @@
   <div class="task">
     <h3>{{ task.title }}</h3>
     <div class="icons">
-      <i class="material-icons ">delete</i>
-      <i class="material-icons ">favorite</i>
+      <i class="material-icons" @click="taskStore.deleteTask(task.id)">delete</i>
+      <i :class="{active: task.isFav}" class="material-icons" @click="taskStore.toggleFav(task.id)">favorite</i>
     </div>
   </div>
 </template>
 
 <script setup>
-defineProps(["task"]);
+import {useTaskStore} from "@/stores/TaskStore.js";
 
+defineProps(["task"]);
+const taskStore = useTaskStore()
 </script>
